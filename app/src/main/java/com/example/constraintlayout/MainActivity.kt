789f0fa2b58045e05f785860e2c2fbac3b9e3bd4
ISTU-PO-ANDROID <istu.po.2204@gmail.com>
2025-01-10 18:23:55 +0400

    feat: data binding added

--- app/src/main/java/com/example/constraintlayout/MainActivity.kt
+++ app/src/main/java/com/example/constraintlayout/MainActivity.kt
@@ -4,14 +4,19 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.constraintlayout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val chainButton = findViewById<Button>(R.id.chainButton)
+//        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+//        val chainButton = findViewById<Button>(R.id.chainButton)
 
-        chainButton.setOnClickListener{
+        binding.name = "Hello world!"
+
+        binding.chainButton.setOnClickListener{
             val intent = Intent(this, ChainActivity::class.java)
             startActivity(intent)
         }
