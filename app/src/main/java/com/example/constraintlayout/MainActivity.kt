e3bf813c276e2578868f65a0780d84d5e92699cf
ISTU-PO-ANDROID <istu.po.2204@gmail.com>
2025-01-10 18:10:16 +0400

    feat: call ChainActivity added

--- app/src/main/java/com/example/constraintlayout/MainActivity.kt
+++ app/src/main/java/com/example/constraintlayout/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.constraintlayout
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val chainButton = findViewById<Button>(R.id.chainButton)
+
+        chainButton.setOnClickListener{
+            val intent = Intent(this, ChainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
